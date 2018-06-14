c6654649c668c20788527117afaae5b4bb0426a9
Jarda Snajdr <jsnajdr@gmail.com>
2018-06-14 12:59:54 +0200

    Print spaces around Flow type parameters

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -2838,7 +2838,7 @@ function printPathNoParens(path, options, print, args) {
       }
 
       if (needsParens) {
-        parts.push("(");
+        parts.push("(", parenSpace);
       }
 
       parts.push(
@@ -2862,7 +2862,7 @@ function printPathNoParens(path, options, print, args) {
         );
       }
       if (needsParens) {
-        parts.push(")");
+        parts.push(parenSpace, ")");
       }
 
       return group(concat(parts));
@@ -4365,7 +4365,7 @@ function printFunctionParams(path, print, options, expandArg, printTypeParams) {
 
   if (isFlowShorthandWithOneArg) {
     if (options.arrowParens === "always") {
-      return concat(["(", concat(printed), ")"]);
+      return concat(["(", parenSpace, concat(printed), parenSpace, ")"]);
     }
     return concat(printed);
   }
@@ -4665,6 +4665,8 @@ function printTypeScriptModifiers(path, options, print) {
 }
 
 function printTypeParameters(path, options, print, paramsKey) {
+  const parenSpace = options.parenSpacing ? " " : "";
+  const parenLine = options.parenSpacing ? line : softline;
   const n = path.getValue();
 
   if (!n[paramsKey]) {
@@ -4692,7 +4694,7 @@ function printTypeParameters(path, options, print, paramsKey) {
         n[paramsKey][0].type === "NullableTypeAnnotation"));
 
   if (shouldInline) {
-    return concat(["<", join(", ", path.map(print, paramsKey)), ">"]);
+    return concat(["<", parenSpace, join(", ", path.map(print, paramsKey)), parenSpace, ">"]);
   }
 
   return group(
@@ -4700,7 +4702,7 @@ function printTypeParameters(path, options, print, paramsKey) {
       "<",
       indent(
         concat([
-          softline,
+          parenLine,
           join(concat([",", line]), path.map(print, paramsKey))
         ])
       ),
@@ -4709,7 +4711,7 @@ function printTypeParameters(path, options, print, paramsKey) {
           ? ","
           : ""
       ),
-      softline,
+      parenLine,
       ">"
     ])
   );
