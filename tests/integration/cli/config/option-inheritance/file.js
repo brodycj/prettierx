498fa5ceb95643b87414b4a6cd238247f4d99409
Adaline Simonian <adalinesimonian@gmail.com>
2021-05-28 14:27:23 -0700

    test: add option inheritance test

---
+++ tests/integration/cli/config/option-inheritance/file.js
@@ -0,0 +1,7 @@
+import {name} from 'package';
+
+function optionInheritanceJs() {
+  const obj = { x: 1 };
+  console.log("option-inheritance/file.js should have no spacing in object brackets");
+  console.log("option-inheritance/file.js should have spacing in import brackets");
+}
---
+++ tests/integration/cli/config/option-inheritance/prettier.config.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = {
+  bracketSpacing: true,
+  objectCurlySpacing: false,
+};
