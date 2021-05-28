39a91609ae30941bc4fc73681c562c1e20bfc969
Adaline Simonian <adalinesimonian@gmail.com>
2021-05-28 11:54:01 -0700

    fix(tests/html): expect vue parser to throw error

--- tests/format/html/html-void-tags/jsfmt.spec.js
+++ tests/format/html/html-void-tags/jsfmt.spec.js
@@ -1,3 +1,6 @@
-run_spec(__dirname, ["html", "angular", "vue"]);
-run_spec(__dirname, ["html"], { htmlVoidTags: true });
-run_spec(__dirname, ["angular", "vue"], { htmlVoidTags: true });
+run_spec(__dirname, ["html", "angular", "vue"], { errors: { vue: true } });
+run_spec(__dirname, ["html"], { htmlVoidTags: true, errors: { vue: true } });
+run_spec(__dirname, ["angular", "vue"], {
+  htmlVoidTags: true,
+  errors: { vue: true },
+});
