46955f273b425fb13e4e6cb48b7d8d23cef4b48e
Adaline Simonian <adalinesimonian@gmail.com>
2021-05-31 13:22:51 -0700

    fix(ci/tests): install optional deps when testing

--- scripts/install-prettierx.js
+++ scripts/install-prettierx.js
@@ -4,9 +4,27 @@ const path = require("path");
 const shell = require("shelljs");
 const tempy = require("tempy");
 
+// [prettierx]: typescript/flow-parser optional dep support
+const pkg = require("../package.json");
+
 shell.config.fatal = true;
 
 const client = process.env.NPM_CLIENT || "yarn";
+
+// [prettierx]: typescript/flow-parser optional dep support
+function getInstallCommand(args) {
+  switch (client) {
+    case "npm":
+      // npm fails when engine requirement only with `--engine-strict`
+      return `npm install ${args} --engine-strict`;
+    case "pnpm":
+      // Note: current pnpm can't work with `--engine-strict` and engineStrict setting in `.npmrc`
+      return `pnpm add ${args}`;
+    default:
+      // yarn fails when engine requirement not compatible by default
+      return `yarn add ${args}`;
+  }
+}
 
 module.exports = (packageDir) => {
   const tmpDir = tempy.directory();
@@ -15,22 +33,22 @@ module.exports = (packageDir) => {
   const tarPath = path.join(tmpDir, file);
 
   shell.exec(`${client} init -y`, { cwd: tmpDir, silent: true });
-  let installCommand = "";
-  switch (client) {
-    case "npm":
-      // npm fails when engine requirement only with `--engine-strict`
-      installCommand = `npm install "${tarPath}" --engine-strict`;
-      break;
-    case "pnpm":
-      // Note: current pnpm can't work with `--engine-strict` and engineStrict setting in `.npmrc`
-      installCommand = `pnpm add "${tarPath}"`;
-      break;
-    default:
-      // yarn fails when engine requirement not compatible by default
-      installCommand = `yarn add "${tarPath}"`;
+
+  // [prettierx]: typescript/flow-parser optional dep support
+  shell.exec(getInstallCommand(`"${tarPath}"`), { cwd: tmpDir });
+
+  // [prettierx]: typescript/flow-parser optional dep support
+  const optionalDeps = new Set()
+
+  for (const dependency of Object.keys(pkg.peerDependenciesMeta)) {
+    if (pkg.peerDependenciesMeta[dependency].optional) {
+      optionalDeps.add(dependency);
+    }
   }
 
-  shell.exec(installCommand, { cwd: tmpDir });
+  if (optionalDeps.size > 0) {
+    shell.exec(getInstallCommand([...optionalDeps].join(" ")), { cwd: tmpDir });
+  }
 
   // [prettierx]
   return path.join(tmpDir, "node_modules/prettierx");
