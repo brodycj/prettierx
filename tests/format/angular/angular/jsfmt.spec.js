9016a72b4dc05c2598f297d946139e7dc1a6447e
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-02 16:04:23 -0400

    FIX-UP: update tests/format/angular/angular/jsfmt.spec.js to improve consistency with Prettier 2.3.0

--- tests/format/angular/angular/jsfmt.spec.js
+++ tests/format/angular/angular/jsfmt.spec.js
@@ -3,13 +3,8 @@ run_spec(__dirname, ["angular"]);
 run_spec(__dirname, ["angular"], { printWidth: 1 });
 run_spec(__dirname, ["angular"], { htmlWhitespaceSensitivity: "ignore" });
 run_spec(__dirname, ["angular"], {
-  // [prettierx]: broken-out bracket spacing options
-  graphqlCurlySpacing: false,
-  exportCurlySpacing: false,
-  importCurlySpacing: false,
+  // [prettierx] bogus option to keep snapshot more consistent with Prettier 2.3.0
+  bogus: null,
+  // [prettierx] updated option:
   objectCurlySpacing: false,
-  typeCurlySpacing: false,
-  yamlBracketSpacing: false,
 });
-// [prettierx]: broken-out bracket spacing options
-run_spec(__dirname, ["angular"], { objectCurlySpacing: false });
