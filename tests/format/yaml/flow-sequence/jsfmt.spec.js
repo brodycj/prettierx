478256e9a152f983a9bea1b8e0c8b85a9f8464aa
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-02 16:35:00 -0400

    [FIX-UP] update tests/format/yaml/flow-sequence/jsfmt.spec.js to improve consistenty of snapshot with Prettier 2.3.0

--- tests/format/yaml/flow-sequence/jsfmt.spec.js
+++ tests/format/yaml/flow-sequence/jsfmt.spec.js
@@ -1,4 +1,9 @@
 run_spec(__dirname, ["yaml"]);
 run_spec(__dirname, ["yaml"], { tabWidth: 4 });
-// [prettierx]: broken-out bracket spacing options
-run_spec(__dirname, ["yaml"], { yamlBracketSpacing: false });
+// [prettierx]: broken-out bracket spacing option(s)
+run_spec(__dirname, ["yaml"], {
+  // [prettierx] bogus option to keep snapshot more consistent with Prettier 2.3.0
+  bogus: null,
+  // [prettierx] replaced bracketSpacing option
+  yamlBracketSpacing: false,
+});
