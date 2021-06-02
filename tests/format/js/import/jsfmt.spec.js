ead7682ae072fb8d91eee22bda76a4bcc2034ca1
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-02 09:47:13 -0400

    use updated option in tests/format/js/import

--- tests/format/js/import/jsfmt.spec.js
+++ tests/format/js/import/jsfmt.spec.js
@@ -1,2 +1,6 @@
 run_spec(__dirname, ["babel", "flow", "typescript"]);
-run_spec(__dirname, ["babel", "flow", "typescript"], { bracketSpacing: false });
+run_spec(__dirname, ["babel", "flow", "typescript"], {
+  // [prettierx] updated option:
+  importCurlySpacing: false,
+});
+
