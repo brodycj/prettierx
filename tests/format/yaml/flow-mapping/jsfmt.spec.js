f7a3a3bdd99280c772dc4b8de94290771f78fddd
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-02 09:48:50 -0400

    update tests/format/yaml/flow-mapping
    - remove `bracketSpacing: false` case
    - make snapshot more consistent with Prettier 2.3.0

--- tests/format/yaml/flow-mapping/jsfmt.spec.js
+++ tests/format/yaml/flow-mapping/jsfmt.spec.js
@@ -1,4 +1,8 @@
 run_spec(__dirname, ["yaml"]);
 run_spec(__dirname, ["yaml"], { tabWidth: 4 });
-run_spec(__dirname, ["yaml"], { bracketSpacing: false });
-run_spec(__dirname, ["yaml"], { yamlBracketSpacing: false });
+run_spec(__dirname, ["yaml"], {
+  // [prettierx] bogus option to keep snapshot more consistent with Prettier 2.3.0
+  bogus: null,
+  // [prettierx] replaced bracketSpacing option
+  yamlBracketSpacing: false,
+});
