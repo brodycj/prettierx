ca32226134aa64d4e1e1b295e65ef526e362de19
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-07 12:55:10 -0400

    prettierx test: move some prettierX-specific test variations
    to help avoid conflicts with coming merge from upstream Prettier

---
+++ tests/x-binary-expressions-with-inner-spacing/jsfmt.spec.js
@@ -0,0 +1,13 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/../binary-expressions`;
+
+run_spec(dirpath, ["babel", "babel-flow", "flow", "typescript"], {
+  // [prettierx] test with --paren-spacing
+  spaceInParens: true,
+  computedPropertySpacing: true,
+  spaceUnaryOps: true,
+  templateCurlySpacing: true,
+});
