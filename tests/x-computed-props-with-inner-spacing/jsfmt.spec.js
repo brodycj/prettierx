1b6dbb8b599cb61cf897ce42ca1701c71575454a
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-09 14:24:06 -0400

    prettierx test: move some prettierX-specific test variations
    to help avoid conflicts with coming merge from upstream Prettier

---
+++ tests/x-computed-props-with-inner-spacing/jsfmt.spec.js
@@ -0,0 +1,9 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/../computed_props`;
+
+run_spec(dirpath, ["babel", "babel-flow"], {
+  computedPropertySpacing: true,
+});
---
+++ tests/x-jsx-with-inner-spacing/jsfmt.spec.js
@@ -0,0 +1,19 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/../jsx`;
+
+run_spec(dirpath, ["babel", "babel-flow", "flow", "typescript"], {
+  singleQuote: false,
+  jsxSingleQuote: false,
+  // [prettierx] test with --paren-spacing
+  spaceInParens: true,
+  arrayBracketSpacing: true,
+  computedPropertySpacing: true,
+  templateCurlySpacing: true,
+  typeAngleBracketSpacing: true,
+  // recommended:
+  arrowParens: "avoid",
+  trailingComma: "none", // ("Standard JS")
+});
