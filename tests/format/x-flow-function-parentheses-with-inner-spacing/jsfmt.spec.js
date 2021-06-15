b4e9601ffc49c81fe6bf388eb5a1b8c61eb9ddc8
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-15 18:29:14 -0400

    prettierx test: move some prettierX-specific test variations
    to help avoid problematic conflicts & lint issues when merging updates
    from upstream Prettier

---
+++ tests/format/x-flow-function-parentheses-with-inner-spacing/jsfmt.spec.js
@@ -0,0 +1,13 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/../../flow_function_parentheses`;
+
+// [prettierx] test with --paren-spacing, only with defaults
+// including arrowParens: "avoid"
+// (note that this combination is **not** recommended)
+run_spec(dirpath, ["flow", "babel", "babel-flow"], {
+  spaceInParens: true,
+  typeAngleBracketSpacing: true,
+});
---
+++ tests/format/x-typescript-conformance-types-mappedType-with-no-type-curly-spacing/jsfmt.spec.js
@@ -0,0 +1,9 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/../../typescript/conformance/types/mappedType`;
+
+run_spec(dirpath, ["typescript"], {
+  typeCurlySpacing: false,
+});
