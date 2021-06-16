cfaefc90b28d50073249d61487e0215ceb4afcb9
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-15 21:32:18 -0400

    prettierx test: revert some Babel parser test updates
    before merging updates from Prettier 2.3.1
    
    (to help avoid some more conflicts)

--- tests/ternaries/jsfmt.spec.js
+++ tests/ternaries/jsfmt.spec.js
@@ -1,11 +1,4 @@
-run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"]);
-run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
-  tabWidth: 4,
-});
-run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
-  useTabs: true,
-});
-run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
-  useTabs: true,
-  tabWidth: 4,
-});
+run_spec(__dirname, ["flow", "typescript"]);
+run_spec(__dirname, ["flow", "typescript"], { tabWidth: 4 });
+run_spec(__dirname, ["flow", "typescript"], { useTabs: true });
+run_spec(__dirname, ["flow", "typescript"], { useTabs: true, tabWidth: 4 });
