10f6fcded0c52a5409e85fe514bdfdecc5619146
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-17 19:11:51 -0400

    ..

--- tests/format/yaml/flow-mapping/jsfmt.spec.js
+++ tests/format/yaml/flow-mapping/jsfmt.spec.js
@@ -1,3 +1,5 @@
 run_spec(__dirname, ["yaml"]);
 run_spec(__dirname, ["yaml"], { tabWidth: 4 });
+// [prettierx] updated option & using bogus option
+// (to keep snapshot more consistent with Prettier 2.3.1):
 run_spec(__dirname, ["yaml"], { bogus: true, yamlBracketSpacing: false });
