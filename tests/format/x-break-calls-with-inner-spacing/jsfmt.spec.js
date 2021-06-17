ea49dc48d78adaf25b5fe4d540c03974ee96e2c3
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-17 01:22:18 -0400

    prettierx test: use plain Babel for break calls test
    (as the main parser)
    
    Co-authored-by: Adaline Valentina Simonian <adalinesimonian@gmail.com>
    Co-authored-by: Christopher J. Brody <chris.brody+brodybits@gmail.com>

--- tests/format/x-break-calls-with-inner-spacing/jsfmt.spec.js
+++ tests/format/x-break-calls-with-inner-spacing/jsfmt.spec.js
@@ -4,7 +4,7 @@
 
 const dirPath = `${__dirname}/../../break-calls`;
 
-run_spec(dirPath, ["babel-flow", "flow", "typescript"], {
+run_spec(dirPath, ["babel", "babel-flow", "flow", "typescript"], {
   // [prettierx] test with --paren-spacing
   spaceInParens: true,
   arrayBracketSpacing: true,
