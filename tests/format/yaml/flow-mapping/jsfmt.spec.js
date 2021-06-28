4b6bc8d964b48ed50a370fe1dfc2f9179ce8a25f
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-28 07:42:04 -0400

    add bogus option to tests/format/yaml/flow-mapping/jsfmt.spec.js

--- tests/format/yaml/flow-mapping/jsfmt.spec.js
+++ tests/format/yaml/flow-mapping/jsfmt.spec.js
@@ -1,3 +1,5 @@
 run_spec(__dirname, ["yaml"]);
 run_spec(__dirname, ["yaml"], { tabWidth: 4 });
-run_spec(__dirname, ["yaml"], { yamlBracketSpacing: false });
+// [prettierx] with yamlBracketSpacing option
+// and bogus option to keep snapshot more consistent with Prettier 2.3.1
+run_spec(__dirname, ["yaml"], { bogus: true, yamlBracketSpacing: false });
